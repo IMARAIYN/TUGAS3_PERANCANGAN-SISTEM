a2de6422d615b4d6abf45dbca9336d867d91fd4f
IRMA NURMALASARI <123700377+IMARAIYN@users.noreply.github.com>
2023-10-20 21:27:55 +0700

    Add files via upload
    TUGAS3_PERANCANGAN SISTEM_IRMA NURMALASARI_20220040127_TI22C

---
+++ tugas3.js
@@ -0,0 +1,18 @@
+//BERTIPE OBJEK
+const Restaurant ={
+    names : "Irma Nurmalasari",
+    city : "Parungkuda",
+    favoriteDrink : "Es krim dan Lemon tea",
+    favoriteFood : "Bakso dan Salad",
+    isVegan : true,
+};
+console.table(Restaurant);
+
+//BERTYPE ARRAY
+const names = ["Irma Nurmalasari"];
+const city = ["Parungkuda"];
+const favoriteDrink =["Es krim dan lemon Tea"];
+const favoriteFood =["Bakso dan salad"]
+const isVegan = ["True"];
+const Restaurantt = [names , city , favoriteDrink , favoriteFood,isVegan];
+console.table(Restaurantt)
